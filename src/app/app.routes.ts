b23fb536fa012273924ab6656bb977c91205767d
Danilo Thiago <danilo.t.a.oliveira@hotmail.com>
2017-11-30 10:59:12 -0300

    teste

--- src/app/app.routes.ts
+++
@@ -1,7 +0,0 @@
-import { Routes } from '@angular/router';
-
-import { IndexComponent } from './index/index.component';
-
-export const ROUTES: Routes = [
-    { path: '', component: IndexComponent }
-]
